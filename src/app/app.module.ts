baca31418305677a448fdb3ebd949c9f1f8c5222
Jayalakshmiraj <Jayalakshmiraj@gmail.com>
2019-05-03 10:58:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,14 +10,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+
+const config = {
+    apiKey: "AIzaSyDTaWAY8TAB3-b5PiTIPwru85HE481OP6c",
+    authDomain: "ionicpush-c6936.firebaseapp.com",
+    databaseURL: "https://ionicpush-c6936.firebaseio.com",
+    projectId: "ionicpush-c6936",
+    storageBucket: "ionicpush-c6936.appspot.com",
+    messagingSenderId: "355979898576",
+    appId: "1:355979898576:web:5548972c3ab2e5b9"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
